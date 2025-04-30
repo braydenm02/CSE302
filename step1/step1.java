fce49afbf8b2cecaf635f1a9b2064d4acd9d8a6f
braydenm02 <braydenmiranda02@gmail.com>
2025-04-30 12:52:33 -0400

    Update step1.java

--- step1/step1.java
+++ step1/step1.java
@@ -21,11 +21,23 @@ public class step1 {
     public static void Block(Scanner scan) {
         String token = scan.next();
 
-        //Acts as a statement list,
-        //parsing the file
+        // Acts as a statement list,
+        // parsing the file
         while (!("end".equals(token))) {
+            statement_list(scan);
             token = scan.next();
         }
     }
 
+    public static void statement_list(Scanner scan) {
+        String token = scan.next();
+
+        if ("forward".equals(token)) {
+
+        } else if ("turn".equals(token)) {
+
+        } else {
+            return;
+        }
+    }
 }
