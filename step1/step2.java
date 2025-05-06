4cba3301b81ccbd4b2a3d68b22cdef0c14ad119e
braydenm02 <99356492+braydenm02@users.noreply.github.com>
2025-05-06 19:02:48 -0400

    Update step2.java

--- step1/step2.java
+++ step1/step2.java
@@ -3,8 +3,10 @@ import java.util.*;
 
 public class step2 {
 
+    //Global Private variables to help with parsing file
     private static Scanner scan;
     private static String token;
+    private static int curNum;
     private static DrawableTurtle turtle;
 
     public static void main(String[] args) {
@@ -38,12 +40,10 @@ public class step2 {
      * Parses the lines between the begin and end token
      */
     public static void block() {
-        System.out.println(token);
         advance(); // Begin token
         while (!("end".equals(token))) {
             statement();
         }
-        System.out.println(token);
         advance(); // End token
     }
 
@@ -54,21 +54,20 @@ public class step2 {
         switch (token) {
             case "forward":
                 advance();
-                System.err.print("forward: ");
-                int distance = number();
-                turtle.forward(distance);
+                curNum = number();
+                turtle.forward(curNum);
+                break;
             case "turn":
                 advance();
-                System.err.print("turn: ");
-                int angle = number();
-                turtle.turn(angle);
+                curNum= number();
+                turtle.turn(curNum);
+                break;
             case "loop":
                 advance();
-                System.err.print("loop: ");
-                int count = number();
-                loop(count);
+                curNum = number();
+                loop(curNum);
+                break;
             case "end":
-                System.err.println("end");
                 break;
             default:
                 throw new IllegalArgumentException("Invalid Command: " + token);
@@ -84,7 +83,6 @@ public class step2 {
         int value = 0;
         if (token.matches("^-?\\d+$")) {
             value = Integer.parseInt(token);
-            System.err.println(value);
             advance();
         } else {
             throw new IllegalArgumentException("Not an Integer: " + token);
@@ -92,13 +90,47 @@ public class step2 {
         return value;
     }
 
+    /**
+     * Runs a nested block of code a certain number of times
+     * @param count Number of repeated runs
+     */
     public static void loop(int count) {
         if (!"begin".equals(token)) {
             throw new IllegalArgumentException("Expected 'begin' after loop count.");
         }
-        for (int i = 0; i < count; i++) {
-            block();
+
+        // Helps with running the loops
+        List<String> toklist = new ArrayList<>();
+        List<Integer> vallist = new ArrayList<>();
+
+
+        //Same functionality as block() but allows us to copy the values into 
+        // the temporary arrays
+        advance(); // Begin Token
+        while (!token.equals("end")) {
+            toklist.add(token);
+            statement();
+            vallist.add(curNum);
         }
+        advance(); //End token
+
+        // The block of code is run once before this looping funcionality, so
+        // we run tthrough the lists count - 1 times
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = 0; j < toklist.size(); j++) {
+                String currtok = toklist.get(j);
+                int curval = vallist.get(j);
+
+                if (currtok.equals("forward")) {
+                    turtle.forward(curval);
+                }
+
+                if (currtok.equals("turn")) {
+                    turtle.turn(curval);
+                }   
+            }
+        }
+        
     }
 
     /**
